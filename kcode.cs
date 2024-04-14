9d85292d1f996e8edad213df97b050f87b9ab00d
X-ERR0R <163181906+X-ERR0R@users.noreply.github.com>
2024-04-14 21:53:38 +0300

    Update alpha

--- kcode.cs
+++ kcode.cs
@@ -16,36 +16,10 @@ namespace progect
                 Console.WriteLine("");
             }
         }
-        static void EDIT_writeMassive(char[] massive)
-        {
-            for (int i = 0; i < massive.Length; i++)
-            {
-                    Console.Write(massive[i]);
-                
-            }
-        }
-        static void EDIT_writeMassive(int[] massive)
-        {
-            for (int i = 0; i < massive.Length; i++)
-            {
-                Console.Write(massive[i]);
-
-            }
-        }
-        static void EDIT_writeMassive(int[,] massive)
-        {
-            for (int i = 0; i < massive.GetLength(0); i++)
-            {
-                for (int j = 0; j < massive.GetLength(1); j++)
-                {
-                    Console.Write(massive[i, j]);
-                }
-                Console.WriteLine("");
-            }
-        }
+        
         static char[,] EDIT_mapFile(string path, int maxRazmerStrocki)
         {
-            var g = File.ReadAllLines(path);
+            string[] g = File.ReadAllLines(path);
             char[,] c = new char[g.Length, maxRazmerStrocki];
             for (int i = 0; i < g.Length; i++)
             {
@@ -56,10 +30,80 @@ namespace progect
             }
             return c;
         }
+  
+       
         static void Main(string[] args)
         {
+            int score = 0;
+            int x = 1, y = 1;
+            Console.CursorVisible = false;
             char[,] map =EDIT_mapFile("map.txt",41);
-            EDIT_writeMassive(map);
+            while (true) 
+            {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Clear(); 
+                EDIT_writeMassive(map);
+                Console.ForegroundColor= ConsoleColor.Yellow;
+                Console.SetCursorPosition(x,y);
+                Console.Write('@');
+                Console.SetCursorPosition(45,0);
+                Console.ForegroundColor=ConsoleColor.White;
+                Console.Write($"у вас {score} очков");
+                ConsoleKeyInfo batton =Console.ReadKey();
+                switch (batton.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if (map[y, x - 1] == '#') { }
+                        else if (map[y,x-1]== '*') 
+                        {
+                            x -= 1;
+                            map[y, x] = ' ';
+                            score++;
+                        }
+                        else
+                        { x -= 1; }
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        if (map[y, x + 1] == '#')
+                        { }
+                        else if (map[y, x + 1] == '*')
+                        {
+                            x += 1;
+                            map[y, x] = ' ';
+                            score++;
+                        }
+                        else
+                        { x += 1; }
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        if (map[y - 1, x] == '#') { }
+                        else if (map[y-1, x] == '*')
+                        {
+                            y -= 1;
+                            map[y, x] = ' ';
+                            score++;
+                        }
+                        else
+                        { y -= 1; }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (map[y + 1, x] == '#')
+                        { }
+                        else if (map[y + 1, x] == '*')
+                        {
+                            y += 1;
+                            map[y, x] = ' ';
+                            score++;
+                        }
+                        else
+                        { y += 1; }
+                        break;
+                }
+
+            }
         }
     }
 }
